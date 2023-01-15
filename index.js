562959fa830f2938b933260aa34ac82b20e550a1
misterbobr <monsieurraspberry@gmail.com>
2023-01-15 20:13:29 +0300

    keyboard controls added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ window.addEventListener('resize', () =>
 });
 
 var X, Y, 
-    moveInterval, rotateLeft, rotateRight, moveForward, moveBackward,
+    moveInterval, rotateLeft, rotateRight, moveForward, moveBackward, moveUp, moveDown,
     angleX = 0,
     angleY = 0,
     camDist = 70,
@@ -56,61 +56,47 @@ function camRotation(dX, dY, camDist) {
     angleX += 2 * Math.PI * dX / window.innerWidth;
     angleY = Math.abs(angleY + 2 * Math.PI * dY / window.innerHeight) < Math.PI / 2
         ? (angleY + 2 * Math.PI * dY / window.innerHeight)
-        : Math.PI / 2 * (angleY > 0 ? 1 : -1);
+        : angleY;
     // angleY += 2 * Math.PI * (mY - Y) / window.innerHeight;
     // console.log(X - mX);
     // console.log(Math.sqrt(Math.pow(camera.position.z, 2) + Math.pow(camera.position.x, 2)));
     let rotX = Math.sin(angleX) * Math.cos(angleY) * camDist;
     let rotY = -Math.sin(angleY) * camDist;
     let rotZ = Math.cos(angleX) * Math.cos(angleY) * camDist;
-    camera.position.x = rotX;
-    camera.position.y = rotY;
-    camera.position.z = rotZ;
+    camera.position.x = rotX + look.x;
+    camera.position.y = rotY + look.y;
+    camera.position.z = rotZ + look.z;
     // camera.lookAt(0, 0, 0);
     camera.lookAt(look);
+    // console.log(look);
     // console.log(Math.sin(2 * Math.PI * (X - mX) / window.innerWidth));
     // console.log('X: ' + (angleX / Math.PI).toFixed(2) + ' pi');
     // console.log('Y: ' + (angleY / Math.PI).toFixed(2) + ' pi');
 };
 
-function camPosition(dX, dY, dZ) {
-    // let direction = new THREE.Vector3(0, 0, -camera.position.z);
-    // direction = direction.applyMatrix4(camera.matrixWorld);
-    // console.log(direction);
-    // camera.getWorldDirection(direction);
-    // console.log(direction);
-    // let dist = Math.sqrt(mX*mX + mY*mY);
-    // console.log('1:');
-    // console.log(direction);
-    // if (dZ === 0) {
-        dZ = -(Math.sin(angleX) * dX + Math.sin(angleY) * Math.cos(angleX) * dY) * 0.05;// + Math.sin(angleY) * (mY - Y)) * 0.02;
-        dX = (Math.cos(angleX) * dX - Math.sin(angleY) * Math.sin(angleX) * dY) * 0.05;
-        dY = -(Math.cos(angleY) * dY) * 0.05;
-    // }
-    // else {
-    //     dX = (Math.sin(angleX) * dZ) * 0.05;
-    //     dZ = -(Math.cos(angleX) * dZ) * 0.05;
-    //     dY = 0;
-    // }
-    console.log(dX);
-    console.log(dY);
-    console.log(dZ);
-    // console.log('aX: ' + angleX);
-    // console.log('aY: ' + angleY);
-    // console.log(angleX, (mX - X));
-    // console.log(angleY, (mY - Y));
-    // direction.x += dX;
-    // direction.z += dZ;
+function camPositionKey(horizontal, vertical) {
+    let dX = (-Math.sin(angleX) * horizontal) * 0.05;
+    let dZ = (-Math.cos(angleX) * horizontal) * 0.05;
+    look.x += dX;
+    look.y += vertical * 0.05;
+    look.z += dZ;
+    camera.position.x += dX;
+    camera.position.y += vertical * 0.05;
+    camera.position.z += dZ;
+    camera.lookAt(look);
+}
+
+function camPositionMouse(dX, dY, dZ) {
+    dZ = -(Math.sin(angleX) * dX + Math.sin(angleY) * Math.cos(angleX) * dY) * 0.05;// + Math.sin(angleY) * (mY - Y)) * 0.02;
+    dX = (Math.cos(angleX) * dX - Math.sin(angleY) * Math.sin(angleX) * dY) * 0.05;
+    dY = -(Math.cos(angleY) * dY) * 0.05;
     look.x += dX;
     look.y += dY;
     look.z += dZ;
-    camera.position.x += dX;// + Math.sin(angleY) * (dY - Y)) * 0.02;
+    camera.position.x += dX;
     camera.position.y += dY;
-    camera.position.z += dZ; // + Math.sin(angleY) * (dY - Y)) * 0.02;
-    // // camera.lookAt(direction);
+    camera.position.z += dZ;
     camera.lookAt(look);
-    // // console.log('2:');
-    // console.log(look);
 }
 
 window.addEventListener('keydown', (e) =>
@@ -118,7 +104,7 @@ window.addEventListener('keydown', (e) =>
     switch (e.code) {
         case 'KeyA':
             if (!rotateLeft) {
-                angleX += 2 * Math.PI * 1 / window.innerWidth;
+                angleX += 2 * Math.PI * 2 / window.innerWidth;
                 rotateLeft = setInterval(() => {
                             camRotation(1, 0, camDist);
                             // mesh.rotateY(2 * Math.PI / window.innerWidth)
@@ -127,7 +113,7 @@ window.addEventListener('keydown', (e) =>
             break;
         case 'KeyD':
             if (!rotateRight) {
-                angleX -= 2 * Math.PI * 1 / window.innerWidth;
+                angleX -= 2 * Math.PI * 2 / window.innerWidth;
                 rotateRight = setInterval(() => {
                             camRotation(-1, 0, camDist);
                             // mesh.rotateY(-2 * Math.PI / window.innerWidth)
@@ -137,19 +123,34 @@ window.addEventListener('keydown', (e) =>
         case 'KeyW':
             if (!moveForward) {
                 moveForward = setInterval(() => {
-                            camPosition(0, 0, 1);
+                            camPositionKey(2, 0);
                         }, 4);
-                break;
             }
+            break;
         case 'KeyS':
             if (!moveBackward) {
                 moveBackward = setInterval(() => {
-                            camRotation(0, 0, -1);
+                            camPositionKey(-2, 0);
                         }, 4);
-                break;
             }
-        // default:
-    }
+            break;
+        case 'Space':
+            if (!moveUp) {
+                moveUp = setInterval(() => {
+                            camPositionKey(0, 2);
+                        }, 4);
+            }
+            break;
+        case 'ShiftLeft':
+            if (!moveDown) {
+                moveDown = setInterval(() => {
+                            camPositionKey(0, -2);
+                        }, 4);
+            }
+            break;
+            // default:
+        }
+        // console.log(e.code);
 });
 
 window.addEventListener('mousedown', (e) => 
@@ -170,7 +171,7 @@ window.addEventListener('mousedown', (e) =>
 
     if (e.button === 2) {
         moveInterval = setInterval(() => {
-            camPosition((mX - X), (mY - Y));
+            camPositionMouse((mX - X), (mY - Y), 0);
             mX = X;
             mY = Y;
         }, 4);
@@ -181,7 +182,7 @@ window.addEventListener('mousedown', (e) =>
 window.addEventListener('keyup', (e) =>
 {
     switch (e.code) {
-        case 'KeyA':      
+        case 'KeyA':     
                 clearInterval(rotateLeft);
                 rotateLeft = null;
                 break;
@@ -196,6 +197,14 @@ window.addEventListener('keyup', (e) =>
         case 'KeyS':
                 clearInterval(moveBackward);
                 moveBackward = null;
+                break;
+        case 'Space':      
+                clearInterval(moveUp);
+                moveUp = null;
+                break;
+        case 'ShiftLeft':
+                clearInterval(moveDown);
+                moveDown = null;
                 break;
     }
 });
@@ -216,7 +225,7 @@ window.addEventListener('wheel', (e) => {
     let delta = e.deltaY * 0.02;
     // if (!distInterval) {
     let distInterval = setInterval(() => {
-        if (Math.abs(delta) > 0.01) {// && camDist + delta > 20) {
+        if (Math.abs(delta) > 0.01 && camDist + delta > 20) {
             camDistance(delta);
             delta *= 0.9;
         }
